216aa855b918f4f6da604a895be3d38d5bcf68d8
ECWireless <elliott@coopallc.com>
2025-07-15 12:33:22 -0600

    feat: add update username func to smart contracts

--- packages/contracts/src/codegen/world/IAdminSystem.sol
+++ packages/contracts/src/codegen/world/IAdminSystem.sol
@@ -32,7 +32,7 @@ interface IAdminSystem {
 
   function app__updateSellEmitterAddress(address sellEmitterAddress) external;
 
-  function app__updateUsername(address playerAddress, string memory newUsername) external;
+  function app__adminUpdateUsername(address playerAddress, string memory newUsername) external;
 
   function app__grantElectricityToTopKingdoms() external;
 
